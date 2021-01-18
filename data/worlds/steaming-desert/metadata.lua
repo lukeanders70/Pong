0bda4e233d3e3d14325d015ed0ac4770e0634c62
lukeanders70 <luke.larson@berkeley.edu>
2021-01-18 12:10:23 -0800

    fixed level locations world 1

--- data/worlds/steaming-desert/metadata.lua
+++ data/worlds/steaming-desert/metadata.lua
@@ -2,17 +2,17 @@ local data = {
     name = "Steaming Desert",
     map = "world1",
     nodes = {
-        {type = "level", x = 30, y = 30, id = 1},
-        {type = "level", x = 40, y = 30, id = 2},
-        {type = "corner", x = 30, y = 40},
-        {type = "level", x = 40, y = 40, id = 3},
-        {type = "level", x = 50, y = 40, id = 4}
+        {type = "level", x = 110, y = 167, id = 1},
+        {type = "level", x = 161, y = 167, id = 2},
+        {type = "corner", x = 214, y = 167},
+        {type = "level", x = 214, y = 145, id = 3},
+        {type = "level", x = 214, y = 223, id = 4}
     },
     paths = {
         {1, 2},
-        {1, 3},
+        {2, 3},
         {3, 4},
-        {4, 5}
+        {3, 5}
     }
 }
 
