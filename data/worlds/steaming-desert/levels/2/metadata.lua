837963a4fe67f55b6ec6f932ab728fe77c5e5e2b
lukeanders70 <luke.larson@berkeley.edu>
2021-01-24 14:45:39 -0800

    added level wrap

--- data/worlds/steaming-desert/levels/2/metadata.lua
+++ data/worlds/steaming-desert/levels/2/metadata.lua
@@ -1,9 +1,11 @@
 local data = {
     name = "Flat Lands",
     playerStart = {x = 2, y = 1},
-    endBell = {x = 29, y = 1},
+    endBell = {x = 74, y = 5},
     background = "background-1",
-    midgrounds = { { name="midground-2", paralaxDivider= 3}, { name="midground-1", paralaxDivider= 2} }
+    midgrounds = { { name="midground-2", paralaxDivider= 3}, { name="midground-1", paralaxDivider= 2} },
+    segmentHeight = 8,
+    segmentLength = 26
 }
 
 return data
