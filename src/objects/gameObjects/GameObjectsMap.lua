09566086638acca3bdc39e60db98637fb7f2c867
lukeanders70 <luke.larson@berkeley.edu>
2021-02-19 20:32:23 -0800

    completed most of level 2

--- src/objects/gameObjects/GameObjectsMap.lua
+++ src/objects/gameObjects/GameObjectsMap.lua
@@ -1,7 +1,6 @@
 local GameObjectsMap = {}
 
 function GameObjectsMap.loadClassFromName(gameObjectName)
-    print(gameObjectName)
     local gameObjectNameCap = gameObjectName:gsub("^%l", string.upper)
     local path = "src/objects/gameObjects/" .. gameObjectNameCap .. ".lua"
     local gameObjectClass = assert( love.filesystem.load(path) )()
