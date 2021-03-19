444776dc8f7e18b50cc648e264d707b7f17aa44e
lukeanders70 <luke.larson@berkeley.edu>
2021-03-18 18:21:45 -0700

    added textures for vertigo and straightshooter

--- src/characters/StraightShooterLeft.lua
+++ src/characters/StraightShooterLeft.lua
@@ -1,10 +1,13 @@
 local TurretType = require('src/characters/TurretType')
+local ObjectTextureIndex = require('src/textures/ObjectTextureIndex')
 
 local StraightShooterLeft = Class{__includes = TurretType}
 
 StraightShooterLeft.NUM_BOUNCES = 0
 function StraightShooterLeft:init(indexX, indexY)
     TurretType.init(self, indexX, indexY, 16, 16, {fireDirection = {x = -1, y = 0}})
+    self.image = ObjectTextureIndex.getImage('straighShooter', self.width, self.height)
+    self:flipHorizontal()
 end
 
 return StraightShooterLeft
