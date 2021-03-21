4a21aa6673d21fa66628bcd067e6a4f84da85c1c
lukeanders70 <luke.larson@berkeley.edu>
2021-03-21 16:31:14 -0700

    added more to level 4

--- data/worlds/steaming-desert/levels/4/1/metadata.lua
+++ data/worlds/steaming-desert/levels/4/1/metadata.lua
@@ -1,10 +1,12 @@
 local data = {
     name = "House",
     playerStart = {x = 2, y = 1},
-    endBell = {x = 23+26+26, y = 7},
     background = "background-2",
     midgrounds = { { name="midground-2-1", paralaxDivider= 3}, { name="midground-2-2", paralaxDivider= 2} },
-    segmentHeight = 13,
+    gameObjects = {
+        ["00"] = {objectType = "well", parameters = {subLevelId = 2, playerPosition = {x = 3, y = 4}}},
+    },
+    segmentHeight = 16,
     segmentLength = 26
 }
 
