c66806c3de8bb175de02c11770562e5196271aaa
lukeanders70 <luke.larson@berkeley.edu>
2021-03-31 19:29:32 -0700

    finished level 4

--- data/worlds/steaming-desert/levels/4/2/metadata.lua
+++ data/worlds/steaming-desert/levels/4/2/metadata.lua
@@ -5,7 +5,7 @@ local data = {
     gameObjects = {
         ["00"] = {objectType = "wellBelow", parameters = {subLevelId = 1, playerPosition = {x = 278, y = 2}}},
         ["01"] = {objectType = "heartContainer", parameters = {}},
-        ["02"] = {objectType = "wellBelow", parameters = {subLevelId = 3, playerPosition = {x = 2, y = 2}}},
+        ["02"] = {objectType = "wellBelow", parameters = {subLevelId = 3, playerPosition = {x = 2, y = 7}}},
     },
     segmentHeight = 17,
     segmentLength = 26,
